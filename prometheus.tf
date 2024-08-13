b48ab93732c0ad51621815583ec1a7b716c35e35
Diego Amorim <diego.amorim@outlook.com>
2024-08-12 23:13:01 -0300

    adicionando código prometheus_ecs

--- prometheus.tf
+++ prometheus.tf
@@ -50,7 +50,7 @@ resource "aws_ecs_task_definition" "prometheus" {
         logDriver = "awslogs"
         options = {
           awslogs-group         = aws_cloudwatch_log_group.prometheus_logs.name
-          awslogs-region        = "us-west-2"
+          awslogs-region        = "us-east-1"
           awslogs-stream-prefix = "prometheus"
         }
       }
